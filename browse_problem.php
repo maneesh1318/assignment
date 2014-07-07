3988a33a5acc73c2512076bd380515d053fa4d5d
maneesh1318 <maneesh1318@gmail.com>
2014-07-07 02:07:51 -0700

    Update browse_problem.php

--- browse_problem.php
+++ browse_problem.php
@@ -17,6 +17,7 @@ Number of Solution:
 <?php
 session_start();
 include 'connect.php';
+
 if(isset($_SESSION['user'])){
 		$uname=$_SESSION['user'];
 	//echo "You Are Already Logged In";
@@ -26,11 +27,12 @@ else{
 }
 $sql = "select * from problem where username!='".$uname."' order by probid";
 $result = mysqli_query($con,$sql);
-$bt="<button>Solve</button>";
+$bt="<button >Solve</button>";
 while($row = mysqli_fetch_array($result)) {
 $pid=$row['probid'];
 $un=$row['username'];
 $st=$row['statement'];
+$bt="<button onclick=\"solve(".$pid.")\" >Solve</button>";
 $n=0;
 echo "<tr><td>".$pid."</td><td>".$st."</td><td>".$un."</td><td>".$n."</td><td>".$bt."</td></tr>";
 
